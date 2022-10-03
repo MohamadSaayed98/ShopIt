4d2a5351e7d3440b1ffa7b4d844487cd1336a6d3
Mohamad <mohamadsaayed1998@gmail.com>
2022-10-03 17:10:48 +0300

    fixing more bugs

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,8 @@ const dotenv = require("dotenv");
 const path = require("path");
 
 //setting up config file
-dotenv.config({ path: "backend/config/config.env" });
+if (process.env.NODE_ENV !== "PRODUCTION")
+  require("dotenv").config({ path: "backend/config/config.env" });
 
 app.use(express.json());
 app.use(bodyparser.urlencoded({ extended: true }));
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 const app = require("./app");
-const dotenv = require("dotenv");
+// const dotenv = require("dotenv");
 const cloudinary = require("cloudinary");
 
 const connectDatabase = require("./config/database");
@@ -13,8 +13,8 @@ process.on("uncaughtException", (err) => {
 });
 
 //setting up config file
-if (process.env.NODE_ENV === "PRODUCTION")
-  dotenv.config({ path: "backend/config/config.env" });
+if (process.env.NODE_ENV !== "PRODUCTION")
+  require("dotenv").config({ path: "backend/config/config.env" });
 
 //connecting to the database
 connectDatabase();
